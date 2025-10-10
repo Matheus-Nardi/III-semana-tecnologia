bf587facfcb88d2bdd842c2b63544ed6a501e2bb
Matheus Nardi <matheuznardi@gmail.com>
2025-10-10 14:35:09 -0300

    feat: adiciona Vercel Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ import Subscription from "@/components/project/Subscription";
 import News from "@/components/project/News";
 import { ScrollToTop } from "@/components/ui/scroll-to-top";
 import ClientErrorHandler from "./client-error-handler";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const montserrat = Montserrat({
   variable: "--font-montserrat",
@@ -22,7 +23,7 @@ const montserrat = Montserrat({
 });
 
 const poppins = Poppins({
-  variable: "--font-poppins", 
+  variable: "--font-poppins",
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
   display: "swap",
@@ -35,7 +36,7 @@ const dmSans = DM_Sans({
   display: "swap",
 });
 
-export const metadata: Metadata = { 
+export const metadata: Metadata = {
   title: "III Semana de Ciência,Tecnologia e Inovação - UNITINS",
   description: "Evento acadêmico promovido pela Universidade Estadual do Tocantins (UNITINS) que reúne estudantes, pesquisadores e profissionais para discutir avanços em ciência, tecnologia e inovação.",
 };
@@ -46,32 +47,32 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-      <html lang="pt-br">
-        <head>
-          <meta name="apple-mobile-web-app-title" content="III SCTI UNITINS" />
-          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
-        </head>
-        <body
-          className={`${montserrat.variable} ${poppins.variable} ${dmSans.variable} antialiased`}
-        >
-          <ClientErrorHandler />
-          <Header /> 
-          <div className="pt-0">
-            <Hero/>
-            {/* <Countdown/> */}
-            <main id="main-content">
-              <AboutEvent/>
-              <Schedule/>
-              <Partners/>
-              {children}
-              <News/>
-              <Faq/>
-              <Location/>
-            </main>
-            <ScrollToTop/>
-          </div>
-          <Footer/>
-        </body>
-      </html>
+    <html lang="pt-br">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="III SCTI UNITINS" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
+      </head>
+      <body
+        className={`${montserrat.variable} ${poppins.variable} ${dmSans.variable} antialiased`}
+      >
+        <ClientErrorHandler />
+        <Header />
+        <div className="pt-0">
+          <Hero />
+          <main id="main-content">
+            <AboutEvent />
+            <Schedule />
+            <Partners />
+            {children}
+            <News />
+            <Faq />
+            <Location />
+          </main>
+          <ScrollToTop />
+        </div>
+        <Footer />
+        <SpeedInsights />
+      </body>
+    </html>
   );
 }
