638fdd207f81aea566b51335557fc319b409f3f5
Matheus Nardi <matheuznardi@gmail.com>
2025-10-16 22:22:52 -0300

    feat: atualiza web manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,6 +37,20 @@ const dmSans = DM_Sans({
 export const metadata: Metadata = {
   title: "III Semana de Ciência,Tecnologia e Inovação - UNITINS",
   description: "Evento acadêmico promovido pela Universidade Estadual do Tocantins (UNITINS) que reúne estudantes, pesquisadores e profissionais para discutir ciência, tecnologia e inovação.",
+  icons:{
+    icon: [
+      { url: "/favicon_io/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon_io/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon_io/favicon.ico", sizes: "any" }
+    ],
+    shortcut: "/favicon_io/favicon.ico",
+    apple: "/favicon_io/apple-touch-icon.png",
+    other: [
+      { rel: "android-chrome-192x192", url: "/favicon_io/android-chrome-192x192.png" },
+      { rel: "android-chrome-512x512", url: "/favicon_io/android-chrome-512x512.png" }
+    ]
+  },
+  manifest: "/favicon_io/site.webmanifest",
   metadataBase: new URL("https://unitinscti.com.br"),
   alternates: { canonical: "/" },
   keywords: [
@@ -55,7 +69,14 @@ export const metadata: Metadata = {
     url: "https://unitinscti.com.br/",
     siteName: "III Semana de Ciência, Tecnologia e Inovação - UNITINS",
     locale: "pt_BR",
-    images: ["/logos/logo-snct.png"], 
+    images: [
+      {
+        url: "/logos/logo-snct.png",
+        width: 1200,
+        height: 630,
+        alt: "Logo da III Semana de Ciência, Tecnologia e Inovação - UNITINS"
+      }
+    ], 
   },
   robots: {
     index: true,
@@ -71,10 +92,18 @@ export const metadata: Metadata = {
     description: "Evento acadêmico — palestras, oficinas, exposições e ciência aplicada.",
     images: ["/logos/logo-snct.png"],
   },
+  other: {
+    "theme-color": "#083D77",
+  },
 };  
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-br">
+      <head>
+        <meta name="theme-color" content="#083D77" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      </head>
       <body
         className={`${montserrat.variable} ${poppins.variable} ${dmSans.variable} antialiased min-h-screen flex flex-col`}
       >
